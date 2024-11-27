432a52d928a6c888cfcb0b4b3421e20b16c7c5a3
Rachel Sansom <eers@leeds.ac.uk>
2024-11-27 10:19:22 +0000

    Adds a TypeError check on daily_min

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,9 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
-from inflammation.models import daily_min
-from inflammation.models import daily_max
+from inflammation.models import daily_mean, daily_min, daily_max
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -43,6 +42,14 @@ def test_daily_min():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
 
+
+def test_daily_min_string():
+    """Test that the min function fails if passed a string"""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min(np.array([["1", "2"],
+                                             ["3", "4"],
+                                             ["5", "6"]]))
 
 def test_daily_max():
     """Test that the max of array is correct"""
